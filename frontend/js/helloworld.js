7b1b35f445b9443affbd73ffbd65e012281f74da
Dan LaFeir <danlafeir@gmail.com>
2016-06-24 20:59:10 -0400

    Dan | Add more to helloworld page
    I added a message to the helloworld page to provide more examples of
    things you might want to do on a webpage. This one demonstrates making
    text a little more fun and dynamic

--- frontend/js/helloworld.js
+++ frontend/js/helloworld.js
@@ -1,3 +1,9 @@
 'use strict'
 
-$(document).ready(function(){});
+$(document).ready(function(){
+	$('#hiddenMessageBtn').click(function(val){
+		var name = $('#name').val();
+		$('#hiddenMessage').show();
+		$('#insertName').html(name);
+	});
+});
